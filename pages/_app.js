bc4de451b329d53aeaf2440d2e319398cc1ad0de
Emidio Valeretto <53981181+emidiovaleretto@users.noreply.github.com>
2024-11-24 15:26:34 +0000

    config: build Tailwind css dependencies

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import "../styles/globals.css";
+
+function Home() {
+  return (
+    <div className="grid place-content-center min-h-screen bg-indigo-950">
+      <h1 className="text-gray-800 bg-gray-200 py-2 px-4 rounded-full text-3xl font-bold">
+        Que bom você por aqui! 🙂
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
--- pages/index.js
+++
@@ -1,5 +0,0 @@
-function Home() {
-  return <h1>Que bom você por aqui!</h1>;
-}
-
-export default Home;
