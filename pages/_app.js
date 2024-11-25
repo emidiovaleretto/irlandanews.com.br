71baf29d0d8eea925cdc2c64cface1ac5afc723a
Emidio Valeretto <53981181+emidiovaleretto@users.noreply.github.com>
2024-11-25 18:49:38 +0000

    chore: create `under construction` layout

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,15 @@ import "../styles/globals.css";
 
 function Home() {
   return (
-    <div className="grid place-content-center min-h-screen bg-indigo-950">
-      <h1 className="text-gray-800 bg-gray-200 py-2 px-4 rounded-full text-3xl font-bold">
-        Que bom você por aqui! 🙂
-      </h1>
+    <div className="grid place-content-center min-h-screen bg-gradient-to-b from-[#4e89a2] to-[#70babd]">
+      <figure>
+        <img
+          className="rounded-full shadow-lg shadow-white/20"
+          src="https://i.ibb.co/CM835pH/DALL-E-2024-11-25-15-17-57-A-vibrant-and-modern-Under-Construction-illustration-for-a-website-titled.webp"
+          alt="DALL-E-2024-11-25-15-17-57-A-vibrant-and-modern-Under-Construction-illustration-for-a-website-titled"
+          border="0"
+        />
+      </figure>
     </div>
   );
 }
