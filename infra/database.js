88339abccd643f9d272187d0c050c1a1d30b8e72
Emidio Valeretto <53981181+emidiovaleretto@users.noreply.github.com>
2024-12-03 19:54:49 +0000

    chore: add a more robust `SSL` configuration

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV == "development" ? false : true,
+    ssl: getSSLValue(),
   });
 
   try {
@@ -25,3 +25,7 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function getSSLValue() {
+  return process.env.NODE_ENV == "production" ? true : false;
+}
