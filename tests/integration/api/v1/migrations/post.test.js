81962e41f8a1e7a550a06ae565fb95b8b1949333
Emidio Valeretto <53981181+emidiovaleretto@users.noreply.github.com>
2024-12-07 19:38:55 +0000

    chore: tidy up `/api/v1/migrations` endpoint

---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,14 @@
+import database from "infra/database";
+
+beforeAll(database.clearDatabase);
+
+test("POST to /api/v1/migrations should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+
+  const responseBody = await response.json();
+  expect(response.status).toBe(201);
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThanOrEqual(1);
+});
