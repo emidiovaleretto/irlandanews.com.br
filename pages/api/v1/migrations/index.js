9690e253df936240fc001f7750c7bcaf7b51553d
Emidio Valeretto <53981181+emidiovaleretto@users.noreply.github.com>
2024-12-08 19:48:01 +0000

    fix migrations endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -7,36 +7,44 @@ export default async function migrations(request, response) {
   if (!allowedMethods.includes(request.method)) {
     return response
       .json({
-        error: "Method not allowed",
+        error: `Method "${request.method.toUpperCase()}" not allowed`,
       })
       .status(405)
       .end();
   }
-  const dbClient = await database.getNewClient();
-  const defaultMigrations = {
-    dbClient: dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
-  if (request.method === "GET") {
-    const pendingMigrations = await migrationRunner(defaultMigrations);
-    dbClient.end();
-    return response.status(200).json(pendingMigrations);
-  }
-  if (request.method == "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrations,
-      dryRun: false,
-    });
 
-    dbClient.end();
-
-    if (migratedMigrations.length > 0) {
-      return response.status(201).json(migratedMigrations);
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
+    const defaultMigrations = {
+      dbClient: dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrations);
+      dbClient.end();
+      return response.status(200).json(pendingMigrations);
     }
+    if (request.method == "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrations,
+        dryRun: false,
+      });
+
+      dbClient.end();
+
+      if (migratedMigrations.length > 0) {
+        return response.status(201).json(migratedMigrations);
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    throw error;
+  } finally {
+    await dbClient.end();
   }
-  return response.status(405).end();
 }
