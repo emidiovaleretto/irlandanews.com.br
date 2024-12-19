3512d2329e3f776a4ee0a218a1b665ec1681836b
Emidio Valeretto <53981181+emidiovaleretto@users.noreply.github.com>
2024-12-19 11:21:36 +0000

    test: rename all descriptions to use new patterns

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,13 +6,36 @@ beforeAll(async () => {
   database.clearDatabase();
 });
 
-test("POST to /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Retrieving pending migrations", () => {
+      test("when running for the first time should return 201", async () => {
+        const response1 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
 
-  const responseBody = await response.json();
-  expect(response.status).toBe(201);
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThanOrEqual(1);
+        const response1Body = await response1.json();
+        expect(response1.status).toBe(201);
+        expect(Array.isArray(response1Body)).toBe(true);
+        expect(response1Body.length).toBeGreaterThanOrEqual(1);
+      });
+
+      test("when running for the second time should return 200", async () => {
+        const response2 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+
+        const response2Body = await response2.json();
+        expect(response2.status).toBe(200);
+        expect(Array.isArray(response2Body)).toBe(true);
+        expect(response2Body.length).toBe(0);
+      });
+    });
+  });
 });
