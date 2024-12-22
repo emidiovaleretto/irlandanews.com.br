3284f2a46de325a5965626e860c1db3bc69160e3
Emidio Valeretto <53981181+emidiovaleretto@users.noreply.github.com>
2024-12-22 15:20:38 +0000

    fix: change `class` to `className` in `Content.js` and fix `Maintanence Icon` image

--- components/status/Content.js
+++ components/status/Content.js
@@ -10,55 +10,55 @@ export default function Content() {
     "Opened Connections": 1,
   };
   return (
-    <div class="relative mt-6 overflow-x-auto shadow-md">
-      <table class="w-full text-sm text-left rtl:text-right text-gray-500 border border-gray-200">
-        <thead class="text-xs text-gray-700 bg-gray-100 border-b border-gray-200">
+    <div className="relative mt-6 overflow-x-auto shadow-md">
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500 border border-gray-200">
+        <thead className="text-xs text-gray-700 bg-gray-100 border-b border-gray-200">
           <tr>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" className="px-6 py-3">
               Current Status by Service
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" className="px-6 py-3">
               <div className="flex items-center justify-center gap-2">
                 <div className="flex items-center">
                   <CheckIcon />
-                  <span class="ml-2">No Issues</span>
+                  <span className="ml-2">No Issues</span>
                 </div>
                 <div className="flex items-center">
                   <MaintanenceIcon />
-                  <span class="ml-2">Maintanence</span>
+                  <span className="ml-2">Maintanence</span>
                 </div>
                 <div className="flex items-center">
                   <IncidentIcon />
-                  <span class="ml-2">Incident</span>
+                  <span className="ml-2">Incident</span>
                 </div>
                 <div className="flex items-center">
                   <OutageIcon />
-                  <span class="ml-2">Outage</span>
+                  <span className="ml-2">Outage</span>
                 </div>
               </div>
             </th>
           </tr>
         </thead>
         <tbody>
-          <tr class="bg-white border-b border-gray-200">
-            <td class="px-6 py-4 font-semibold">Database Version</td>
-            <td class="flex justify-end items-center px-6 py-4">
+          <tr className="bg-white border-b border-gray-200">
+            <td className="px-6 py-4 font-semibold">Database Version</td>
+            <td className="flex justify-end items-center px-6 py-4">
               <span className="w-12 rounded-md text-center text-sm bg-green-200 px-3 py-2 text-green-950">
                 {data["Database Version"]}
               </span>
             </td>
           </tr>
-          <tr class="bg-white border-b border-gray-200">
-            <td class="px-6 py-4 font-semibold">Max Connections</td>
-            <td class="flex justify-end items-center px-6 py-4">
+          <tr className="bg-white border-b border-gray-200">
+            <td className="px-6 py-4 font-semibold">Max Connections</td>
+            <td className="flex justify-end items-center px-6 py-4">
               <span className="w-12 rounded-md text-center text-sm bg-green-200 px-3 py-2 text-green-950">
                 {data["Max Connections"]}
               </span>
             </td>
           </tr>
-          <tr class="bg-white border-b border-gray-200">
-            <td class="px-6 py-4 font-semibold">Opened Connections</td>
-            <td class="flex justify-end items-center px-6 py-4">
+          <tr className="bg-white border-b border-gray-200">
+            <td className="px-6 py-4 font-semibold">Opened Connections</td>
+            <td className="flex justify-end items-center px-6 py-4">
               <span className="w-12 rounded-md text-center text-sm bg-green-200 px-3 py-2 text-green-950">
                 {data["Opened Connections"]}
               </span>
