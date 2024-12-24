945e088759e811ad1a9a9c80801fc88af728bd5e
Emidio Valeretto <53981181+emidiovaleretto@users.noreply.github.com>
2024-12-24 14:28:14 +0000

    refactor: retrieve database status information dynamically

--- pages/status/index.js
+++ pages/status/index.js
@@ -1,7 +1,7 @@
 import Title from "../../components/status/Title";
 import UpdatedAt from "../../components/status/UpdatedAt";
 import CheckIcon from "../../components/icons/CheckIcon";
-import Content from "../../components/status/Content";
+import DatabaseStatus from "../../components/status/Content";
 
 export default function StatusPage() {
   return (
@@ -9,7 +9,7 @@ export default function StatusPage() {
       <CheckIcon className="w-32 h-32" />
       <Title />
       <UpdatedAt />
-      <Content />
+      <DatabaseStatus />
     </div>
   );
 }
